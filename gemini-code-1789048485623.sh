81cb4a942257c9c39addb774692526a65c223df7
השם שלך < >
2026-09-10 16:56:04 +0300

    העלאת מאגר הספרים

---
+++ gemini-code-1789048485623.sh
@@ -0,0 +1,7 @@
+cd /d "C:\Users\y0258\Desktop\ספרים נוספים לאוצריא\ים החכמה"
+git init
+git add .
+git commit -m "העלאת מאגר הספרים"
+git branch -M main
+git remote add origin https://github.com/torahtyh/yam-HaHachma.git
+git push -u origin main
